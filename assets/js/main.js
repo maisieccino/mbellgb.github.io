b8322d8a5d9dfedf4cd8b7a2fdaef6b77fd9103d
Matthew Bell <logicnotlogical@outlook.com>
2016-12-19 02:01:26 +0000

    Update theme to OG

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,10 +2,37 @@ $(function() {
     FastClick.attach(document.body);
 });
 
-$('.jumbo').each(function () {
-    var imgUrl = $(this).data('img');
-    if (imgUrl) {
-        console.log(imgUrl);
-        $(this).css("background-image", "url("+imgUrl+")");
-    }
+// $('.jumbo').each(function () {
+//     var imgUrl = $(this).data('img');
+//     if (imgUrl) {
+//         console.log(imgUrl);
+//         $(this).css("background-image", "url("+imgUrl+")");
+//     }
+// });
+
+
+// This fixes the weird page jumping thing on mobile browsers.
+// http://stackoverflow.com/questions/24944925/background-image-jumps-when-address-bar-hides-ios-android-mobile-chrome
+var jumbo = $('.jumbo-home');
+jQuery(window).resize("resizeBackground");
+function resizeBackground() {
+    jumbo.height(jQuery(window).height());
+}
+resizeBackground();
+
+$('#entry-button').click(function() {
+    console.log("ouch");
+    $('html, body').animate({
+        scrollTop: $('.main').offset().top
+    }, 1000);
+});
+
+$('.menu-button').click(function() {
+    $(this).toggleClass('active');
+    $('body').toggleClass('active');
+});
+
+$('.container').click(function () {
+    $('.menu-button').removeClass('active');
+    $('body').removeClass('active');
 });
